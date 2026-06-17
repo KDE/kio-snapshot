6e64d43fca21591838002c01f3e0114ce587e9e3
Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
2026-06-17 16:54:23 +0530

    Getting snapshots by file and subvolume

---
+++ ksnapshotservice.cpp
@@ -0,0 +1,298 @@
+#include "ksnapshotservice.h"
+
+#include <QCoreApplication>
+#include <QDBusConnection>
+#include <QDBusConnectionInterface>
+#include <QDBusError>
+#include <QDir>
+#include <QFileInfo>
+#include <QString>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include <btrfsutil.h>
+
+using namespace Qt::StringLiterals;
+
+int openDirForUser(const QString &path, uint userId)
+{
+    // we use a file descriptor here to avoid time-of-check-to-time-of-use problems
+    // since various functions in libbtrfsutil can take the same directory fd that we check
+    int fd = open(path.toUtf8().constData(), O_RDONLY | O_DIRECTORY);
+    struct stat sb;
+    if (fd == -1 || fstat(fd, &sb) == -1 || sb.st_uid != userId) {
+        if (fd != -1) {
+            close(fd);
+        }
+        return -1;
+    }
+
+    return fd;
+}
+
+std::optional<QString> getRootSubvolumePath()
+{
+    enum btrfs_util_error btrfs_err;
+
+    char *root_path;
+    btrfs_err = btrfs_util_subvolume_get_path("/", 0, &root_path);
+    if (btrfs_err) {
+        return std::nullopt;
+    }
+
+    QString result = QString::fromUtf8(root_path);
+    free(root_path);
+    return result;
+}
+
+std::optional<QString> getAbsoluteSubvolumePath(uint64_t subvolume)
+{
+    enum btrfs_util_error btrfs_err;
+
+    char *path;
+    btrfs_err = btrfs_util_subvolume_get_path("/", subvolume, &path);
+    if (btrfs_err) {
+        return std::nullopt;
+    }
+
+    QString subvolumePath = QString::fromUtf8(path);
+    free(path);
+
+    std::optional<QString> rootPath = getRootSubvolumePath();
+    if (!rootPath.has_value()) {
+        return std::nullopt;
+    }
+
+    if (!subvolumePath.startsWith(rootPath.value())) {
+        return std::nullopt;
+    }
+
+    QString subvolumePathAbs = subvolumePath.sliced(rootPath.value().length());
+    return subvolumePathAbs;
+}
+
+std::optional<QVariantList> getSnapshotsForSubvolume(uint64_t subvolume, uint userId)
+{
+    QVariantList snapshots;
+    enum btrfs_util_error btrfs_err;
+
+    std::optional<QString> subvolumePathAbs = getAbsoluteSubvolumePath(subvolume);
+    if (!subvolumePathAbs.has_value()) {
+        return std::nullopt;
+    }
+
+    int subvolumeDirFd = openDirForUser(subvolumePathAbs.value(), userId);
+    if (subvolumeDirFd == -1) {
+        return std::nullopt;
+    }
+    close(subvolumeDirFd);
+
+    struct btrfs_util_subvolume_info info;
+    btrfs_err = btrfs_util_subvolume_get_info("/", subvolume, &info);
+    if (btrfs_err) {
+        return std::nullopt;
+    }
+    QByteArrayView subvolumeUuid = QByteArrayView::fromArray(info.uuid);
+
+    struct btrfs_util_subvolume_iterator *it;
+    btrfs_err = btrfs_util_subvolume_iter_create("/", subvolume, 0, &it);
+    if (btrfs_err) {
+        return std::nullopt;
+    }
+
+    struct btrfs_util_subvolume_info iter_info;
+    char *iter_path;
+    while (!(btrfs_err = btrfs_util_subvolume_iter_next_info(it, &iter_path, &iter_info))) {
+        if (QByteArrayView::fromArray(iter_info.parent_uuid) == subvolumeUuid) {
+            // (parent_uuid == subvolume_uuid) => this is a snapshot of the subvolume
+            std::optional<QString> snapshotPathOpt = ::getAbsoluteSubvolumePath(iter_info.id); // QString::fromUtf8(iter_path);
+            if (snapshotPathOpt.has_value()) {
+                QString snapshotPath = snapshotPathOpt.value();
+                int snapshotDirFd = openDirForUser(snapshotPath, userId);
+                if (snapshotDirFd != -1) {
+                    close(snapshotDirFd);
+                    // user owns snapshot dir, so we can reveal it
+                    QVariantMap snapshot;
+                    snapshot["SubvolumeId"_L1] = QVariant::fromValue<qulonglong>(static_cast<qulonglong>(iter_info.id));
+                    snapshot["Path"_L1] = snapshotPath;
+                    snapshot["CreationTimeSec"_L1] = QVariant::fromValue<qulonglong>(static_cast<qulonglong>(iter_info.ctime.tv_sec));
+                    snapshot["CreationTimeNanosec"_L1] = QVariant::fromValue<qulonglong>(static_cast<qulonglong>(iter_info.ctime.tv_sec));
+
+                    snapshots << snapshot;
+                }
+            }
+        }
+        free(iter_path);
+    }
+    btrfs_util_subvolume_iter_destroy(it);
+
+    return snapshots;
+}
+
+KSnapshotService::KSnapshotService(QObject *parent)
+    : QObject(parent)
+{
+}
+
+QDBusReply<uint> KSnapshotService::getUserId()
+{
+    QDBusConnection conn = connection();
+    QDBusMessage msg = message();
+    const QString &service = msg.service();
+    return conn.interface()->serviceUid(service);
+}
+
+qulonglong KSnapshotService::getSubvolumeForPath(const QString &path)
+{
+    QDBusReply<uint> userIdReply = getUserId();
+    if (!userIdReply.isValid()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return 0;
+    }
+    uint userId = userIdReply.value();
+
+    int dirFd = openDirForUser(path, userId);
+    if (dirFd == -1) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return 0;
+    }
+
+    uint64_t subvolumeId;
+    enum btrfs_util_error btrfs_err = btrfs_util_subvolume_get_id_fd(dirFd, &subvolumeId);
+    if (btrfs_err) {
+        sendErrorReply(QDBusError::InternalError, QString::fromUtf8(btrfs_util_strerror(btrfs_err)));
+    }
+    close(dirFd);
+    return subvolumeId;
+}
+
+QString KSnapshotService::getPathForSubvolume(qulonglong subvolume)
+{
+    QDBusReply<uint> userIdReply = getUserId();
+    if (!userIdReply.isValid()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QString();
+    }
+    uint userId = userIdReply.value();
+
+    std::optional<QString> subvolumePathAbs = getAbsoluteSubvolumePath(subvolume);
+
+    if (!subvolumePathAbs.has_value()) {
+        sendErrorReply(QDBusError::InternalError);
+        return QString();
+    }
+
+    int dirFd = openDirForUser(subvolumePathAbs.value(), userId);
+    if (dirFd == -1) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QString();
+    }
+    close(dirFd);
+
+    return subvolumePathAbs.value();
+}
+
+QVariantList KSnapshotService::getSnapshotsForSubvolume(qulonglong subvolume)
+{
+    QDBusReply<uint> userIdReply = getUserId();
+    if (!userIdReply.isValid()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+    uint userId = userIdReply.value();
+
+    std::optional<QVariantList> snapshots = ::getSnapshotsForSubvolume(subvolume, userId);
+    if (!snapshots.has_value()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+
+    return snapshots.value();
+}
+
+QVariantList KSnapshotService::getSnapshotsForFile(const QString &path)
+{
+    QDBusReply<uint> userIdReply = getUserId();
+    if (!userIdReply.isValid()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+    uint userId = userIdReply.value();
+
+    QFileInfo fileInfo(path);
+    QDir parentDir = fileInfo.absoluteDir();
+
+    if (fileInfo.ownerId() != userId) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+
+    int parentDirFd = openDirForUser(parentDir.absolutePath(), userId);
+    if (parentDirFd == -1) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+    uint64_t subvolume;
+    enum btrfs_util_error btrfs_err = btrfs_util_subvolume_get_id_fd(parentDirFd, &subvolume);
+    if (btrfs_err) {
+        sendErrorReply(QDBusError::AccessDenied);
+    }
+    close(parentDirFd);
+
+    std::optional<QString> subvolumeRootOpt = ::getAbsoluteSubvolumePath(subvolume);
+    if (!subvolumeRootOpt.has_value()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+    QDir subvolumeRoot(subvolumeRootOpt.value());
+
+    QString pathRel = subvolumeRoot.relativeFilePath(path);
+
+    std::optional<QVariantList> snapshotsOpt = ::getSnapshotsForSubvolume(subvolume, userId);
+    if (!snapshotsOpt.has_value()) {
+        sendErrorReply(QDBusError::AccessDenied);
+        return QVariantList();
+    }
+    QVariantList snapshots = snapshotsOpt.value();
+
+    QVariantList fileSnapshots;
+    for (const QVariant &snapshotVar : snapshots) {
+        QVariantMap snapshot = snapshotVar.toMap();
+        QVariantMap fileSnapshot;
+        QString fileSnapshotPath = QDir(snapshot["Path"_L1].toString()).absoluteFilePath(pathRel);
+        QFileInfo fileSnapshotInfo(fileSnapshotPath);
+        if (!fileSnapshotInfo.exists() || fileSnapshotInfo.ownerId() != userId) {
+            continue;
+        }
+        fileSnapshot["Path"_L1] = fileSnapshotPath;
+        fileSnapshot["SnapshotCreationTimeSec"_L1] = snapshot["CreationTimeSec"_L1];
+        fileSnapshot["SnapshotCreationTimeNanosec"_L1] = snapshot["CreationTimeNanosec"_L1];
+        fileSnapshot["ModificationTimeSec"_L1] = fileSnapshotInfo.lastModified().toSecsSinceEpoch();
+        fileSnapshots << fileSnapshot;
+    }
+    return fileSnapshots;
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication app(argc, argv);
+
+    QDBusConnection bus = QDBusConnection::systemBus();
+
+    if (!bus.registerService("org.kde.ksnapshotservice"_L1)) {
+        qCritical() << "Failed to register D-Bus service name:" << bus.lastError().message();
+        return 1;
+    }
+
+    KSnapshotService service;
+
+    if (!bus.registerObject("/KSnapshotService"_L1, &service, QDBusConnection::ExportScriptableContents)) {
+        qCritical() << "Failed to export object on path:" << bus.lastError().message();
+        return 1;
+    }
+
+    qDebug() << "KSnapshotService is running on D-Bus...";
+    return app.exec();
+}
---
+++ ksnapshotservice.h
@@ -0,0 +1,25 @@
+#include <QDBusConnection>
+#include <QDBusContext>
+#include <QDBusMessage>
+#include <QDBusReply>
+#include <QObject>
+
+#pragma once
+
+class KSnapshotService : public QObject, protected QDBusContext
+{
+    Q_OBJECT
+    Q_CLASSINFO("D-Bus Interface", "org.kde.ksnapshotservice")
+
+public:
+    KSnapshotService(QObject *parent = nullptr);
+
+public Q_SLOTS:
+    Q_SCRIPTABLE qulonglong getSubvolumeForPath(const QString &path);
+    Q_SCRIPTABLE QString getPathForSubvolume(qulonglong subvolume);
+    Q_SCRIPTABLE QVariantList getSnapshotsForSubvolume(qulonglong subvolume);
+    Q_SCRIPTABLE QVariantList getSnapshotsForFile(const QString &path);
+
+private:
+    QDBusReply<uint> getUserId();
+};
