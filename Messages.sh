1299b373af6b6d16e529a747e18eb13b9eba23e7
Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
2026-07-15 18:42:28 +0530

    Add some missing SPDXes, and rename project in CMake

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# SPDX-FileCopyrightText: 2026 Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
+# SPDX-License-Identifier: LGPL-2.0-or-later
+
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/kio_snapshot.pot
