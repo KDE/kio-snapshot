aa7c910768e41013bb550b7861eac0bbf57219ed
Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
2026-07-28 10:42:53 +0530

    Try to build and test on KDE Linux CI image
    ... which allows sudo usage. Thanks to Harald for the suggestion ^^

---
+++ ci_build_test.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2026 Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
+# SPDX-License-Identifier: LGPL-2.0-or-later
+
+set -eux
+
+sudo pacman --sync --refresh --noconfirm btrfs-progs
+mkdir build
+cmake -B build -S . -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Debug
+cmake --build build
+SOURCEDIR=$PWD
+cd /tmp
+bash "$SOURCEDIR/autotests/prep_btrfs.sh"
+cd "$SOURCEDIR"
+export KIO_SNAPSHOT_TEST_MOUNTPOINT=/tmp/butter-tray
+export QML_LOGGING_RULES="default.debug=true"
+cmake --build build --target test
