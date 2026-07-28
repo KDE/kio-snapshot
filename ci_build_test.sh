0222d09bd054dab6fb6f6b1e57f43a3ce6667ab4
Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
2026-07-28 11:13:09 +0530

    ci: kde-linux: install more dependencies

--- ci_build_test.sh
+++ ci_build_test.sh
@@ -5,7 +5,7 @@
 
 set -eux
 
-sudo pacman --sync --refresh --noconfirm btrfs-progs
+sudo pacman --sync --refresh --noconfirm btrfs-progs kio solid ki18n cmake base-devel
 mkdir build
 cmake -B build -S . -DBUILD_TESTING=ON -DCMAKE_BUILD_TYPE=Debug
 cmake --build build
@@ -14,5 +14,5 @@ cd /tmp
 bash "$SOURCEDIR/autotests/prep_btrfs.sh"
 cd "$SOURCEDIR"
 export KIO_SNAPSHOT_TEST_MOUNTPOINT=/tmp/butter-tray
-export QML_LOGGING_RULES="default.debug=true"
+export QT_LOGGING_RULES="default.debug=true"
 cmake --build build --target test
