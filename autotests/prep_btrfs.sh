e16219449c8fef92a4a98e5d666f4791fe67d043
Bharadwaj Raju <bharadwaj.raju@machinesoul.in>
2026-08-15 00:10:03 +0530

    autotests: fix after-recreation subtest
    after-recreation had the same mtime as current, so it wouldn't be
    shown.

--- autotests/prep_btrfs.sh
+++ autotests/prep_btrfs.sh
@@ -25,6 +25,8 @@ sudo btrfs subvolume snapshot butter-tray butter-tray/@after-removal
 echo "again" > butter-tray/file.txt
 sudo btrfs subvolume snapshot butter-tray butter-tray/@after-recreation
 
+echo "fin" > butter-tray/file.txt  # current
+
 sudo btrfs subvolume create butter-tray/sub
 sudo chown -R $USER:$USER butter-tray/sub
 sudo btrfs subvolume snapshot butter-tray/sub butter-tray/@sub-initial
@@ -36,9 +38,8 @@ sudo btrfs subvolume snapshot butter-tray/sub butter-tray/@sub-after-additions
 rm butter-tray/sub/vol.txt
 sudo btrfs subvolume snapshot butter-tray/sub butter-tray/@sub-after-removal
 echo "again" > butter-tray/sub/vol.txt
-sudo btrfs filesystem sync butter-tray
-sudo btrfs filesystem sync butter-tray/sub
-sleep 1
 sudo btrfs subvolume snapshot butter-tray/sub butter-tray/@sub-after-recreation
+
+echo "fin" > butter-tray/sub/vol.txt  # current
 
 sudo btrfs filesystem sync butter-tray
